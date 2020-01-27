a36f00838d6602d356849b8f2b3e030dc39dd6e3
NE-CP2eJr <cp2ejr-ne@inatel.br>
2020-01-27 12:16:53 -0300

    Adicionando projetos

---
+++ README.txt
@@ -0,0 +1,12 @@
+Alterado por: Tiago
+Data: 20/01/2020
+Alterações:
+-Como tirar sua habilitação
+-Serviços oferecidos
+-pop ups de orçamento e contato
+-Logo da CP2
+
+Futuras alterações:
+-Tornar pop ups flutuantes
+-Navegar na página inicial usando o menu
+-avisos
---
+++ assets/js/form.js
@@ -0,0 +1,30 @@
+function  openContForm(){
+    document.getElementById("contactForm").style.display = "block";
+    document.getElementById("contactForm").style.position = "absolute";
+}
+
+function closeContForm(){
+    document.getElementById("contactForm").style.display = "none";
+    document.getElementById("contactForm").style.position = "fixed";
+}
+
+function  openOrcForm(){
+    document.getElementById("orcForm").style.display = "block";
+    document.getElementById("orcForm").style.position = "absolute";
+}
+
+function closeOrcForm(){
+    document.getElementById("orcForm").style.display = "none";
+    document.getElementById("orcForm").style.position = "fixed";
+}
+
+function verificaOrc(){
+    var optionSelected = document.getElementById("service").value;
+    var categoryService = document.getElementById("category");
+    if(optionSelected =="1"||optionSelected=="2"){
+        categoryService.disabled = false;
+    }else{
+        categoryService.value = "0"
+        categoryService.disabled = true;
+    }
+}
